073e7d7410f8c031b14d7fcb1dd3ce759c707245
Bryce Peters <91118447+beters02@users.noreply.github.com>
2023-11-07 14:18:36 -0700

    Update init.lua

--- src/shared/Entities/Chest/init.lua
+++ src/shared/Entities/Chest/init.lua
@@ -1,7 +1,7 @@
 -- [[ Any interactable block is going to be considered a "Chest" ]]
 
 local Chest = {
-    RBXTemplateObjec = game.ReplicatedStorage.Assets.Chest,
+    RBXTemplateObject = game.ReplicatedStorage.Assets.Chest,
     Properties = {IsAlive = false, Health = 50, Drops = true},
     Components = {
         {Template = "Container"}
